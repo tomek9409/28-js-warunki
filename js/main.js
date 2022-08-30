a660b9bd65dfa0e5562242067397ca1d17796d89
Tomek <kolpak9409@gmail.com>
2022-08-30 18:07:19 +0200

    poprawione

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@ if (number > 10) {
   console.log("Podana liczba jest większa od 10");
 } else if (number < 10) {
   console.log("Podana liczba nie jest większa od 10");
+} else if (number === 10) {
+  console.log("Podana liczba jest równa 10");
 }
